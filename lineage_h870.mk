f1f870c6a4eb5067971210514aba23c0ebdf32f9
BernardoBas <bernardobas@gmail.com>
2021-03-05 13:02:48 -0300

    h870: build fingerprint RQ2A.210305.006

--- lineage_h870.mk
+++ lineage_h870.mk
@@ -41,4 +41,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="lucye_global_com" \
     PRIVATE_BUILD_DESC="lucye_global_com-user 9 PKQ1.190522.001 1930916292597 release-keys"
 
-BUILD_FINGERPRINT := "lge/lucye_global_com/lucye:9/PKQ1.190522.001/1930916292597:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ2A.210305.006/7119741:user/release-keys"
