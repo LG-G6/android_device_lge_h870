c6d869fff6203eecfe3ea4376b2612569de89c6f
Bernardo Bas <bernardobas@gmail.com>
2021-12-14 16:53:03 -0300

    h870: Dot-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_h870.mk
+    $(LOCAL_DIR)/dot_h870.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_h870-userdebug \
-    lineage_h870-eng
+    dot_h870-userdebug \
+    dot_h870-user \
+    dot_h870-eng
