f690df8ec086932e2651e2076d1244c0b46b6b02
Bernardo Bas <bernardobas@gmail.com>
2022-04-13 09:47:24 -0300

    h870: Go Havoc!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_h870.mk
+    $(LOCAL_DIR)/havoc_h870.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_h870-userdebug \
-    lineage_h870-eng
+    havoc_h870-userdebug \
+    havoc_h870-user \
+    havoc_h870-eng
--- havoc_h870.mk
+++ havoc_h870.mk
@@ -16,10 +16,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit from h870 device
 $(call inherit-product, device/lge/h870/device.mk)
@@ -30,7 +30,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH_OVERRIDE)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := h870
-PRODUCT_NAME := lineage_h870
+PRODUCT_NAME := havoc_h870
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H870
 PRODUCT_MANUFACTURER := LGE
